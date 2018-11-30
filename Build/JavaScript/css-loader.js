e8e022cff286cf889fffc6443a432a8c4f5715db
Wilhelm Behncke <behncke@sitegeist.de>
2018-11-30 15:01:11 +0100

    FEATURE: Implement css loader

---
+++ Build/JavaScript/css-loader.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const vm = require('vm');
+
+module.exports = function (content) {
+	const basePath = path.join(
+		path.dirname(this.resourcePath),
+		path.basename(this.resourcePath, '.css')
+	);
+
+	if (fs.existsSync(`${basePath}.fusion`)) {
+		const parsedFusionSource = /prototype\(([.:a-zA-Z0-9]*)\)/.exec(
+			fs.readFileSync(`${basePath}.fusion`)
+		);
+
+		if (parsedFusionSource) {
+			const prototypeName = parsedFusionSource[1];
+			const exports = {};
+			const sandbox = {
+				__webpack_public_path__: '', // eslint-disable-line
+				module: {exports},
+				exports
+			};
+
+			vm.createContext(sandbox);
+			vm.runInContext(content.split('// exports')[1], sandbox);
+
+			const additionalFusionSource = `
+prototype(${prototypeName}) {
+	renderer.@context.styles = \${${JSON.stringify(sandbox.module.exports.locals)}}
+}
+			`;
+
+			fs.writeFileSync(`${basePath}.css.fusion`, additionalFusionSource);
+		}
+	}
+
+	return content;
+};
