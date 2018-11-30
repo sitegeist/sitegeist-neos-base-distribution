44cb65855b3085cea4a682d24d88e9ff621a0e32
Wilhelm Behncke <behncke@sitegeist.de>
2018-11-30 15:02:37 +0100

    TASK: Configure postcss

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	plugins: [
+		require('postcss-custom-media'),
+		require('postcss-media-minmax'),
+		require('postcss-mixins'),
+		require('precss'),
+		require('autoprefixer'),
+		require('postcss-custom-properties'),
+		require('postcss-calc'),
+		require('postcss-image-set-polyfill'),
+		require('postcss-selector-not'),
+		require('postcss-discard-duplicates'),
+		require('postcss-object-fit-images')
+	]
+};
