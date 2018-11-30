2f1d9b35e123d282bf0e38a41a0ee52ccf1fa1e5
Wilhelm Behncke <behncke@sitegeist.de>
2018-11-30 15:00:51 +0100

    FEATURE: Implement components loader

---
+++ Build/JavaScript/components-loader.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const glob = require('glob');
+const querystring = require('querystring');
+
+const asArray = value => Array.isArray(value) ? value : [value];
+
+module.exports = function () {
+	const options = Object.assign({
+		componentPaths: [],
+		runtime: ''
+	}, querystring.parse(this.resourceQuery.substr(1)));
+	const componentFileNames = asArray(options.componentPaths)
+		.map(path => {
+			this.addContextDependency(path);
+
+			return `${path}/**/*.+(css|js|fusion)`;
+		})
+		.reduce((files, pattern) => [...files, ...glob.sync(pattern)], [])
+		.filter(fileName => !fileName.endsWith('.css.fusion'))
+		.filter(fileName => !fileName.endsWith('.js.fusion'));
+	const source = [];
+	const components = [];
+
+	for (const fileName of componentFileNames) {
+		const basePath = path.join(
+			path.dirname(fileName),
+			path.basename(fileName, '.fusion')
+		);
+		const importName = `_${basePath.split('/').join('_').split('.').join('_')}`;
+
+		switch (true) {
+			case fileName.endsWith('.fusion'):
+				source.push(`import * as ${importName} from '${fileName}';`);
+				components.push(importName);
+				break;
+
+			case fileName.endsWith('.css'):
+			case fileName.endsWith('.js'):
+			default:
+				source.push(`import '${fileName}';`);
+				break;
+		}
+	}
+
+	source.push(`import runtime from '${options.runtime}';`);
+	source.push('var components = {};');
+
+	for (const component of components) {
+		source.push(`components[${component}.prototypeName] = ${component};`);
+	}
+
+	source.push('runtime(components);');
+
+	return source.join('\n');
+};
