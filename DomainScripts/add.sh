5688e99ba6e326fc9c0bd63e2815130ab62bd36c
Christian Schwahn <schwahn@sitegeist.de>
2019-07-05 09:05:21 +0200

    clean: remove double slash

--- DomainScripts/add.sh
+++ DomainScripts/add.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-source DomainScripts//project.env.makefile
-source DomainScripts//manage-etc-hosts.sh
-source DomainScripts//tools.sh
+source DomainScripts/project.env.makefile
+source DomainScripts/manage-etc-hosts.sh
+source DomainScripts/tools.sh
 
 #### fetch IP addresses of running containers ####
 fetchContainerIPs
--- DomainScripts/remove.sh
+++ DomainScripts/remove.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 source DomainScripts/project.env.makefile
-source DomainScripts//manage-etc-hosts.sh
+source DomainScripts/manage-etc-hosts.sh
 
 #### remove domain to etc/hosts ####
 removehost
