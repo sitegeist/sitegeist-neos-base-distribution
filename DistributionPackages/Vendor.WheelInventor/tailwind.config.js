50ca1afb64d5e2651293e57e956321e72c397f9b
Henrik Radandt <radandt@sitegeist.de>
2021-06-10 15:38:19 +0200

    Add styles for Button variants.

--- DistributionPackages/Vendor.WheelInventor/tailwind.config.js
+++ DistributionPackages/Vendor.WheelInventor/tailwind.config.js
@@ -1,3 +1,5 @@
+const theme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	mode: 'jit',
 	purge: [
@@ -5,6 +7,37 @@ module.exports = {
 	],
 	darkMode: false,
 	theme: {
+		colors: {
+			brand: {
+				DEFAULT: '#564e58',
+				contrast: '#fff'
+			},
+			cta: {
+				DEFAULT: '#ff934f',
+				contrast: '#000'
+			},
+			neutral: {
+				lighter: theme.colors.gray[200],
+				DEFAULT: theme.colors.gray[500],
+				darker: theme.colors.gray[900]
+			},
+			info: {
+				DEFAULT: '#76bed0',
+				contrast: '#000'
+			},
+			error: {
+				DEFAULT: '#f61067',
+				contrast: '#fff'
+			},
+			success: {
+				DEFAULT: '#20fc8f',
+				contrast: '#000'
+			},
+			warning: {
+				DEFAULT: '#f4e04d',
+				contrast: '#000'
+			},
+		},
 		extend: {},
 	},
 	variants: {
