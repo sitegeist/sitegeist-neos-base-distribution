3d8e17fbfd3d430fe57126687c26ecec9c0faedb
Bernhard Schmitt <schmitt@sitegeist.de>
2023-08-23 14:09:10 +0200

    WIP: Adjust to Neos 9

--- DistributionPackages/Vendor.SupportWheelInventor/Resources/Private/Fusion/Integration/SiteFooterFactory.php
+++ DistributionPackages/Vendor.SupportWheelInventor/Resources/Private/Fusion/Integration/SiteFooterFactory.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
 
 namespace Vendor\SupportWheelInventor\Integration;
 
-use Neos\ContentRepository\Domain\Model\Node;
+use Neos\ContentRepository\Core\Projection\ContentGraph\Node;
 use PackageFactory\AtomicFusion\PresentationObjects\Fusion\AbstractComponentPresentationObjectFactory;
 use Vendor\Shared\Presentation\Block\SiteFooter\SiteFooter;
 
